fa46f4a760caf6655ab0a347d69ceaab4db92405
Andrew Sinner <andrewsinner@Andrews-MacBook-Air.local>
2019-03-18 19:33:00 -0700

    Init

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import yargs from 'yargs'
+import bodyParser from 'body-parser'
+
+const PORT = yargs.argv.port || 3000
+const app = express()
+
+
+app.use(bodyParser.json())
+app.get('/', (_, res) => {
+    res.send('Hello, world')
+})
+app.get('/health-check', (req, res) => {
+    res.send('OK')
+})
+app.listen(PORT, () => {
+    console.log(`Listening on 127.0.0.1:${PORT}`)
+})
