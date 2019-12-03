680198f086d36c57c1549993f445b5819b09de10
Andrew Sinner <andrewsinner@gmail.com>
2019-12-03 08:43:54 -0800

    init

--- ormconfig.js
+++ ormconfig.js
@@ -3,14 +3,11 @@ module.exports = {
     host: "localhost",
     port: 3306,
     username: "root",
-    database: "dbname",
+    database: "identity",
     synchronize: true,
     logging: false,
     entities: [
-       "src/orm/models/*.ts"
-    ],
-    migrations: [
-       "src/orm/migration/*.ts"
+       "src/orm/entities/*.ts"
     ],
     bigNumberStrings: true
  }
--- src/orm/createConnection.ts
+++
@@ -1,6 +0,0 @@
-import { getConnectionOptions, createConnection as ormCreateConnection, Connection } from "typeorm";
-
-export async function createConnection(): Promise<Connection> {
-    const options = await getConnectionOptions()
-    return ormCreateConnection(options)
-}
